3865967a56154c55b760f0db57fc55f7b04a8b9c
Michal Charemza <michal@charemza.name>
2020-06-04 07:32:32 +0100

    (build) Initial setup.py skeleton

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='http-signature-server',
+    version='0.0.0',
+    author='Michal Charemza',
+    author_email='michal@charemza.name',
+    description='Implementation of the server side of the IETF draft "Signing HTTP Messages"',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalc/python-http-signature-server',
+    py_modules=[
+        'http_signature_server',
+    ],
+    python_requires='>=3.5.0',
+    install_requires=[
+        'cryptography>=2.9.2,<3',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security :: Cryptography',
+    ],
+)
